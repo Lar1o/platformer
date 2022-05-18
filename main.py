7c195335c844c6b5c49b48ac33b7e41da64331b9
Denis Fateev <denisprof@yandex.ru>
2022-05-18 15:23:59 +0300

    Добавлено столкновение с платформами, оптимизация

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from pygame import *
 from time import time as timer
-import random
+
 
 window = display.set_mode((700,500))
 display.set_caption("Platformer")
-backgr = transform.scale(image.load('background.jpg'), (700,500))
-window.blit(backgr,(0,0))
+backgr = transform.scale(image.load('assets/background.jpg'), (700,500))
+spr_img_R = transform.scale(image.load('assets/right_side.png'), (70,80)) 
+spr_img_L = transform.scale(image.load('assets/left_side.png'), (70,80)) 
 font.init()
 back = (0, 0, 0)
 window.fill(back)
@@ -29,38 +30,79 @@ class GameSprite(sprite.Sprite):
 
 
 class Player(GameSprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
+        super().__init__(player_image, player_x, player_y, player_speed, width, height)
+        self.can_jump = True
+
     def gravity(self):
-        if self.rect.y <= 360:
-            self.speedY -= 0.4
-        else:
+        self.speedY += 0.4
+    
+    def jump(self):
+        self.rect.y += 1
+
+        walls_collided = sprite.spritecollide(self, walls, False)
+
+        if len(walls_collided) > 0 or self.rect.y > 360:
+            self.speedY = -9
+
+        self.rect.y -= 1
+
+    def move(self):
+        self.rect.x += self.speedX
+
+        walls_collided = sprite.spritecollide(self, walls, False)
+        for wall in walls_collided:
+            if self.speedX > 0:
+                self.rect.right = wall.rect.left
+                self.speedX = 0
+            elif self.speedX < 0:
+                self.rect.left = wall.rect.right
+                self.speedX = 0
+
+        self.can_jump = False
+        self.rect.y += self.speedY + 1
+
+        walls_collided = sprite.spritecollide(self, walls, False)
+        for wall in walls_collided:
+            if self.speedY > 0:
+                self.rect.bottom = wall.rect.top
+                self.speedY = 0
+            elif self.speedY < 0:
+                self.rect.top = wall.rect.bottom
+                self.speedY = 0
+        
+        if self.rect.y > 360:
             self.speedY = 0
+            self.rect.y -= self.speedY + 1
 
     def update(self):
-        self.rect.y -= self.speedY
-        self.rect.x += self.speedX
+        self.gravity()
+        self.move()
+        
         keys_pressed = key.get_pressed()
         if keys_pressed[K_d] and self.rect.x < 650:
             if self.speedX < 5:
                 self.speedX += 1
 
             if self.turn == 'left':
-                self.image = transform.scale(image.load('right_side.png'), (70,80)) 
+                self.image = spr_img_R
                 self.turn = 'right'
         elif keys_pressed[K_a] and self.rect.x > 5:
             if self.speedX > -5:
                 self.speedX -= 1
             if self.turn == 'right':
-                self.image = transform.scale(image.load('left_side.png'), (70,80)) 
+                self.image = spr_img_L
                 self.turn = 'left'
         else:
             if self.speedX > 0:
                 self.speedX = 0
             elif self.speedX < 0:
                 self.speedX = 0
-        if keys_pressed[K_SPACE] and self.rect.y > 0 and self.speedY == 0:
-            self.speedY = 8
-            self.rect.y -= self.speedY
-   
+                
+        if keys_pressed[K_SPACE]:
+            self.jump()
+
+
 class Button():
     def __init__(self,x,y,width,height,color,fill_color,text):
         self.x = x
@@ -112,12 +154,20 @@ class Wall(GameSprite):
         super().__init__(player_image, player_x*50, player_y*35, 0, width, height)
 
 
-spr1 = Player('right_side.png', 0, 100, 5, 70, 80)
-button_1 = Button(380,250,180,50,(250, 0, 0),(0, 250, 0, 0),'Уровень 2')
-button_2 = Button(180,100,350,50,(250, 0 , 0),(0, 250, 0, 0),'Выберите уровень')
-button_3 = Button(130,250,180,50,(250, 0 , 0),(0, 250, 0, 0),'Уровень 1')
-button_4 = Button(250,350,180,50,(250, 0 , 0),(0, 250, 0, 0),'Уровень 3')
-enemy_one = Enemy(('rocket.png'),200,200,1,35,45)
+def load_level(level):
+    with open(f'levels/Setka{level}.txt', 'r') as file:
+        data = file.readlines()
+        for i in range(len(data)):
+            for j in range(len(data[i])):
+                if data[i][j] == '1':
+                    walls.add(Wall(('assets/wall.jpg'), j, i, 50, 35))
+
+spr1 = Player('assets/right_side.png', 0, 100, 5, 70, 80)
+button_title = Button(180,100,350,50,(250, 0 , 0),(0, 250, 0, 0),'Выберите уровень')
+button_1 = Button(130,250,180,50,(250, 0 , 0),(0, 250, 0, 0),'Уровень 1')
+button_2 = Button(380,250,180,50,(250, 0, 0),(0, 250, 0, 0),'Уровень 2')
+button_3 = Button(250,350,180,50,(250, 0 , 0),(0, 250, 0, 0),'Уровень 3')
+enemy_one = Enemy(('assets/rocket.png'),200,200,1,35,45)
 enemy_one.direction = 'left'
 
 game = True
@@ -125,76 +175,53 @@ start = False
 
 walls = sprite.Group()
 
-def load_level(level):
-    with open(f'Setka{level}.txt', 'r') as file:
-        data = file.readlines()
-        for i in range(len(data)):
-            for j in range(len(data[i])):
-                if data[i][j] == '1':
-                    walls.add(Wall(('steni 2.jpg'), j, i, 50, 35))
 
-
+''' игровой цикл '''
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-    '''меню'''
+
+    ''' меню '''
     if start == False:
         button_1.reset()
         button_2.reset()
         button_3.reset()
-        button_4.reset()
+        button_title.reset()
 
         if e.type == MOUSEBUTTONDOWN:
                 x,y = e.pos
 
                 if button_1.collidepoint(x,y):
+                    load_level(1)
+                    spr1.rect.y = 150
+                    spr1.rect.x = 0
+                    start = True
+
+                if button_2.collidepoint(x,y):
                     load_level(2)
                     spr1.rect.y = 350
                     spr1.rect.x = 0
                     start = True
-                    
 
                 if button_3.collidepoint(x,y):
-                    load_level(1)
-                    spr1.rect.y = 350
-                    spr1.rect.x = 0
-                    start = True
-
-                if button_4.collidepoint(x,y):
                     load_level(3)
                     spr1.rect.y = 350
                     spr1.rect.x = 0
                     start = True
-    '''игра'''
+    
+    ''' игра '''
     if start:
         window.blit(backgr,(0,0))
         spr1.reset()
         spr1.update()
-        spr1.gravity()
         enemy_one.reset()
         enemy_one.update()
         if sprite.collide_rect(spr1,enemy_one):
             start = False
-        if sprite.spritecollide(spr1, walls, False):
-            wall = sprite.spritecollide(spr1, walls, False)
-            if spr1.speedX > 0:
-                spr1.rect.right = wall[0].rect.left
-            elif spr1.speedY > 0:
-                spr1.rect.bottom = wall[0].rect.top
-        
 
         walls.draw(window)
 
 
     clock.tick(FPS)
     display.update()
-
-
-
-
-
-
-
-
-
